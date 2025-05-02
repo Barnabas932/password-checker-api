f2dbc8d0ca62729f2e44c82672fdb5264877a017
Szigetvári Barnabás <barnabszigetvari@gmail.com>
2025-05-02 12:08:09 +0200

    html modification

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import re
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class PasswordRequest(BaseModel):
     password: str
@@ -17,9 +20,14 @@ def validate_password(pwd: str):
         issues.append("Nagybetűt kell tartalmaznia.")
     if not re.search(r'\d', pwd):
         issues.append("Számot kell tartalmaznia.")
-    if not re.search(r'[!@#$%^&*()_\-+=\[\]{};:\'",.<>?/\\|]', pwd):
+    if not re.search(r'[!@#$%^&*()_\\-+=\\[\\]{};:\\\"<>,.?/\\\\|]', pwd):
         issues.append("Speciális karaktert kell tartalmaznia (!@#$...).")
     return issues
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("static/index.html") as f:
+        return f.read()
 
 @app.post("/check_password")
 def check_password(req: PasswordRequest):
